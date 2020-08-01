884bb2b7e3b23e2455df5faf491fda9b27b100a2
941485-patrol <dribbleroar@gmail.com>
2020-08-01 12:49:39 +0800

    add cors allowing credentials header

--- controllers/user/main.py
+++ controllers/user/main.py
@@ -1,4 +1,4 @@
-from flask import (Blueprint, session, g)
+from flask import (Blueprint, session, g, request)
 from models import User
 
 bp=Blueprint('users', __name__, url_prefix='/users')
@@ -13,7 +13,8 @@ def get_current_user():
 
 @bp.after_app_request
 def add_cors_headers(response):
-    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Origin'] = request.headers.get('Origin')
     response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE'
     response.headers['Access-Control-Allow-Headers'] = '*'
+    response.headers['Access-Control-Allow-Credentials'] = 'true'
     return response
