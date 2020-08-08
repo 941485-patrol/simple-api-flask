adaa3a07bbfa056fd9f9960b9f3ee34f2aaf3a43
941485-patrol <dribbleroar@gmail.com>
2020-08-08 21:03:57 +0800

    update config

--- controllers/user/login.py
+++ controllers/user/login.py
@@ -13,30 +13,12 @@ def login():
     if request.method == 'POST':
         form = LoginForm()
         if form.validate_on_submit():
-            # return jsonify({'message':'You are now logged in.'}), 200
+            return jsonify({'message':'You are now logged in.'}), 200
             # return responser(jsonify({'message':'You are now logged in.'}), 200)
-            res = make_response(jsonify({'message':'You are now logged in.'}), 200)
-            sess_cookie = request.cookies.get('session',None)
-            if sess_cookie is not None:
-                res.headers['Set-Cookie'] = 'session={}; SameSite=None; Secure; HttpOnly; path=/'.format(sess_cookie)
-            return res
+            # res = make_response(jsonify({'message':'You are now logged in.'}), 200)
+            # sess_cookie = request.cookies.get('session',None)
+            # if sess_cookie is not None:
+            #     res.headers['Set-Cookie'] = 'session={}; SameSite=None; Secure; HttpOnly; path=/'.format(sess_cookie)
+            # return res
         else:
             return jsonify({'errors': form.errors}), 400
-        # form = LoginForm()
-        # if form.validate_on_submit():
-        #     username=form.username.data
-        #     password=form.password.data
-        #     isUserExists=User.query.filter_by(username=username).first()
-        #     if isUserExists:
-        #         pwhash=isUserExists.password
-        #         isPassCorrect=check_password_hash(pwhash,password)
-        #         if isPassCorrect==True:
-        #             session.clear()
-        #             session['user_id'] = isUserExists.id
-        #             return jsonify({'message':'You are now logged in.'})
-        #         else:
-        #             return jsonify({'error':'Wrong password.'})
-        #     else:
-        #         return jsonify({'error':'Wrong credentials.'}),400
-        # else:
-        #     return jsonify({'errors': form.errors})
